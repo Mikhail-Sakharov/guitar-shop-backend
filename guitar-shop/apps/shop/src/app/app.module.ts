9c2f05595f3ed243139d5bbdb2582cd3d84d5207
Mikhail-Sakharov <94370679+Mikhail-Sakharov@users.noreply.github.com>
2023-02-12 15:25:29 +0300

    Обновил структуру проекта

--- guitar-shop/apps/shop/src/app/app.module.ts
+++ guitar-shop/apps/shop/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [ProductModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
