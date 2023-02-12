f56b3fe2ca79c340776aa7c70a213c2b0d87fc0f
Mikhail-Sakharov <94370679+Mikhail-Sakharov@users.noreply.github.com>
2023-02-12 15:54:01 +0300

    Установил @nestjs/swagger и class-validator, добавил настройки

--- guitar-shop/apps/shop/src/main.ts
+++ guitar-shop/apps/shop/src/main.ts
@@ -3,13 +3,26 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import {Logger, ValidationPipe} from '@nestjs/common';
+import {NestFactory} from '@nestjs/core';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
-import { AppModule } from './app/app.module';
+import {AppModule} from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('The «Shop» service')
+    .setDescription('Shop service API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
